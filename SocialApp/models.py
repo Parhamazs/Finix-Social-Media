23ab70718498e2cef8500b1d9304046ffa1b1716
Parhamazs <pazizsona@gmail.com>
2024-09-27 00:09:37 +0330

    models
    our core user model created

--- SocialApp/models.py
+++ SocialApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    bio = models.TextField(null=True,blank=True, verbose_name='بایو کاربر')
+    photo = models.ImageField(blank=False,null=False, upload_to="account_images/",verbose_name='تصویر کاربری')
+    phone = models.CharField(max_length=12)
+    birthday = models.DateField()
