49241f7ab9df4317394f3841c52a99c7baec2584
Parhamazs <pazizsona@gmail.com>
2024-10-07 19:03:40 +0330

    models update
    i updated my models just now
    and add some new classes for user post reaction

--- SocialApp/models.py
+++ SocialApp/models.py
@@ -1,9 +1,61 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 # Create your models here.
+
 class User(AbstractUser):
-    bio = models.TextField(null=True,blank=True, verbose_name='بایو کاربر')
-    photo = models.ImageField(blank=False,null=False, upload_to="account_images/",verbose_name='تصویر کاربری')
-    phone = models.CharField(max_length=12)
-    birthday = models.DateField(null=True,blank=True,verbose_name='تاریخ تولد')
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(unique=True)
+    birthday = models.DateField(null=True, blank=True)
+    phone = models.CharField(max_length=20, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    photo = models.ImageField(upload_to='profile_pics', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
+
+class Like(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.post.content}"
+
+class Friendship(models.Model):
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user2')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user1.username} is friends with {self.user2.username}"
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receiver')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.content
--- SocialApp/views.py
+++ SocialApp/views.py
@@ -1,3 +1,103 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Comment, Like, Friendship, Message
+from .forms import PostForm, CommentForm, MessageForm
+
+# Create your views here.
+
+class PostListView(LoginRequiredMixin, ListView):
+    model = Post
+    template_name = 'social_media/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return Post.objects.all().order_by('-created_at')
+
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+    template_name = 'social_media/post_detail.html'
+    context_object_name = 'post'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'social_media/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'social_media/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'social_media/post_confirm_delete.html'
+    success_url = '/social_media/'
+
+class CommentCreateView(LoginRequiredMixin, CreateView):
+    model = Comment
+    form_class = CommentForm
+    template_name = 'social_media/comment_form.html'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        form.instance.post_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+class LikeView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        Like.objects.get_or_create(user=request.user, post=post)
+        return redirect('post_detail', pk=pk)
+
+class UnLikeView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        Like.objects.filter(user=request.user, post=post).delete()
+        return redirect('post_detail', pk=pk)
+
+class FriendshipView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        user2 = User.objects.get(pk=pk)
+        Friendship.objects.get_or_create(user1=request.user, user2=user2)
+        return redirect('profile', pk=pk)
+
+class UnFriendshipView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        user2 = User.objects.get(pk=pk)
+        Friendship.objects.filter(user1=request.user, user2=user2).delete()
+        return redirect('profile', pk=pk)
+
+class MessageCreateView(LoginRequiredMixin, CreateView):
+    model = Message
+    form_class = MessageForm
+    template_name = 'social_media/message_form.html'
+
+    def form_valid(self, form):
+        form.instance.sender = self.request.user
+        form.instance.receiver_id = self.kwargs['pk']
+        return super().form_valid(form)
+
+@login_required
+def profile(request, pk):
+    user = User.objects.get(pk=pk)
+    posts = Post.objects.filter(user=user).order_by('-created_at')
+    friends = user.friendship_set.all()
+    return render(request, 'social_media/profile.html', {'user': user, 'posts': posts, 'friends': friends})
+
+@login_required
+def inbox(request):
+    messages = Message.objects.filter(receiver=request.user).order_by('-created_at')
+    return render(request, 'social_media/inbox.html', {'messages': messages})
 
 # Create your views here.
